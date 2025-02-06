1402a42c8de0dfc13d213b8f3c1305165b90e08a
Alexey Kiselev <czycorp@gmail.com>
2025-02-06 18:55:14 +0300

    Refresh tokens if access token is expired

--- src/utils/api/auth.ts
+++ src/utils/api/auth.ts
@@ -1,6 +1,46 @@
-import { API_AUTH_LOGIN_URL, API_AUTH_LOGOUT_URL, API_AUTH_REGISTER_URL, API_AUTH_USER_INFO_URL } from "../../constants"
+import {
+  API_AUTH_LOGIN_URL,
+  API_AUTH_LOGOUT_URL,
+  API_AUTH_REGISTER_URL,
+  API_AUTH_TOKEN_URL,
+  API_AUTH_USER_INFO_URL,
+} from "../../constants"
 import { IUser } from "../../services/types"
 import { request } from "./common"
+
+export const refreshToken = async () => {
+  const refreshData = await request(API_AUTH_TOKEN_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify({
+      token: localStorage.getItem("refreshToken"),
+    }),
+  })
+  if (!refreshData.success) {
+    return Promise.reject(refreshData)
+  }
+  localStorage.setItem("refreshToken", refreshData.refreshToken)
+  localStorage.setItem("accessToken", refreshData.accessToken)
+  return refreshData
+}
+
+export const fetchWithRefresh = async (url: string, options: RequestInit | undefined = undefined) => {
+  try {
+    const response = await request(url, options)
+    return response
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (err: any) {
+    if (err.message === "jwt expired") {
+      const refreshData = await refreshToken()
+      options = { ...options, headers: { ...options?.headers, Authorization: refreshData.accessToken } }
+      return request(url, options)
+    } else {
+      return Promise.reject(err)
+    }
+  }
+}
 
 export const authRegisterApi = async ({
   email,
@@ -13,7 +53,7 @@ export const authRegisterApi = async ({
 }): Promise<IUser> => {
   const data = await request(API_AUTH_REGISTER_URL, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json;charset=utf-8" },
     body: JSON.stringify({ email: email, password: password, name: name }),
   })
   localStorage.setItem("accessToken", data.accessToken)
@@ -24,9 +64,12 @@ export const authRegisterApi = async ({
 
 export const authGetUser = async (): Promise<IUser> => {
   try {
-    const data = await request(API_AUTH_USER_INFO_URL, {
+    const data = await fetchWithRefresh(API_AUTH_USER_INFO_URL, {
       method: "GET",
-      headers: { "Content-Type": "application/json", Authorization: `${localStorage.getItem("accessToken")}` },
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+        Authorization: `${localStorage.getItem("accessToken")}`,
+      },
     })
     const user: IUser = data.user
     return user
@@ -40,7 +83,7 @@ export const authGetUser = async (): Promise<IUser> => {
 export const authLoginApi = async ({ email, password }: { email: string; password: string }): Promise<IUser> => {
   const data = await request(API_AUTH_LOGIN_URL, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json;charset=utf-8" },
     body: JSON.stringify({ email: email, password: password }),
   })
   localStorage.setItem("accessToken", data.accessToken)
@@ -52,7 +95,7 @@ export const authLoginApi = async ({ email, password }: { email: string; passwor
 export const authLogoutApi = async (): Promise<void> => {
   request(API_AUTH_LOGOUT_URL, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json;charset=utf-8" },
     body: JSON.stringify({ token: localStorage.getItem("refreshToken") }),
   })
   localStorage.removeItem("accessToken")
@@ -68,9 +111,12 @@ export const authUpdateUser = async ({
   email: string
   password: string
 }): Promise<IUser> => {
-  const data = await request(API_AUTH_USER_INFO_URL, {
+  const data = await fetchWithRefresh(API_AUTH_USER_INFO_URL, {
     method: "PATCH",
-    headers: { "Content-Type": "application/json", Authorization: `${localStorage.getItem("accessToken")}` },
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+      Authorization: `${localStorage.getItem("accessToken")}`,
+    },
     body: JSON.stringify({ email: email, password: password, name: name }),
   })
   const user: IUser = data.user
--- src/utils/api/common.ts
+++ src/utils/api/common.ts
@@ -1,5 +1,5 @@
 export const getResponse = (response: Response) => {
-  return response.ok ? response.json() : Promise.reject(response.status)
+  return response.ok ? response.json() : response.json().then((err) => Promise.reject(err))
 }
 
 export const request = (url: string, options: RequestInit | undefined = undefined) => {
